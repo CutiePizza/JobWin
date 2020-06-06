e822e87cf169b51ad32ef81d6e35600b8b92e306
Yasmine  Hamdi <yassyasshamdi@gmail.com>
2020-06-06 12:26:51 +0100

    category

--- models/category.py
+++ models/category.py
@@ -10,7 +10,7 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class Answer(BaseModel, Base):
+class Category(BaseModel, Base):
     """
     category class
 
