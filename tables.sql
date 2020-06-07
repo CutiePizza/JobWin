9cf3b1ae1c117f405f5a54b3e551ae622c6f4408
CutiePizza <ines.chokri97@gmail.com>
2020-06-07 15:03:20 +0100

    Update tables

--- tables.sql
+++ tables.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS job_win_db.Users(
-user_id int,
+id varchar(60) PRIMARY KEY,
+created_at DATETIME,
+updated_at DATETIME,
 name varchar(255),
 last_name varchar(255),
 username varchar(255),
@@ -9,110 +11,135 @@ email varchar(255),
 type varchar(255),
 pic varbinary(5000),
 status varchar(255),
-gender varchar(255),
-PRIMARY KEY(user_id)
+gender varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Category(
-cat_id int PRIMARY KEY,
+id varchar(60) PRIMARY KEY,
+created_at DATETIME,
+updated_at DATETIME,
 name varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Interview(
-int_id int PRIMARY KEY,
+id varchar(60) PRIMARY KEY,
+created_at DATETIME,
+updated_at DATETIME,
 status varchar(255),
 submit_date date,
 type varchar(255),
-user_id int,
-cat_id int,
-FOREIGN KEY (user_id) REFERENCES job_win_db.Users(user_id),
-FOREIGN KEY (cat_id) REFERENCES job_win_db.Category(cat_id)
+user_id varchar(60),
+cat_id varchar(60),
+FOREIGN KEY (user_id) REFERENCES job_win_db.Users(id),
+FOREIGN KEY (cat_id) REFERENCES job_win_db.Category(id)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Subcategory(
-sub_id int PRIMARY KEY,
+id varchar(60) PRIMARY KEY,
+created_at DATETIME,
+updated_at DATETIME,
 name varchar(255),
-cat_id int,
-FOREIGN KEY (cat_id) REFERENCES job_win_db.Category(cat_id)
+cat_id varchar(60),
+FOREIGN KEY (cat_id) REFERENCES job_win_db.Category(id)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Post(
-post_id int PRIMARY KEY,
-created_at date,
-updated_at date,
+id varchar(60) PRIMARY KEY,
+created_at DATETIME,
+updated_at DATETIME,
 content varchar(255),
-sub_id int,
-u_id int,
-cat_id int,
-FOREIGN KEY(u_id) REFERENCES job_win_db.Users(user_id),
-FOREIGN KEY(sub_id) REFERENCES job_win_db.Subcategory(sub_id),
-FOREIGN KEY(cat_id) REFERENCES job_win_db.Category(cat_id)
+sub_id varchar(60),
+u_id varchar(60),
+cat_id varchar(60),
+FOREIGN KEY(u_id) REFERENCES job_win_db.Users(id),
+FOREIGN KEY(sub_id) REFERENCES job_win_db.Subcategory(id),
+FOREIGN KEY(cat_id) REFERENCES job_win_db.Category(id)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Comments(
-com_id int PRIMARY KEY,
-created_at date, 
-updated_at date,
-user_id int,
-post_id int,
-FOREIGN KEY (user_id) REFERENCES job_win_db.Users(user_id),
-FOREIGN KEY (post_id) REFERENCES job_win_db.Post(post_id)
+id varchar(60) PRIMARY KEY,
+created_at DATETIME,
+updated_at DATETIME,
+user_id varchar(60),
+post_id varchar(60),
+FOREIGN KEY (user_id) REFERENCES job_win_db.Users(id),
+FOREIGN KEY (post_id) REFERENCES job_win_db.Post(id)
 );
 
 
 CREATE TABLE IF NOT EXISTS job_win_db.Question(
-q_id int PRIMARY KEY,
+id varchar(60) PRIMARY KEY,
+created_at DATETIME,
+updated_at DATETIME,
 text varchar(255),
 type varchar(255),
-sub_id int,
-FOREIGN KEY (sub_id) REFERENCES job_win_db.Subcategory(sub_id)
+sub_id varchar(60),
+FOREIGN KEY (sub_id) REFERENCES job_win_db.Subcategory(id)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Answer(
-a_id int PRIMARY KEY,
+id int PRIMARY KEY,
+created_at DATETIME,
+updated_at DATETIME,
 text varchar(255),
 audio varbinary(5000),
-int_id int,
-FOREIGN KEY (int_id) REFERENCES job_win_db.Interview(int_id)
+int_id varchar(60),
+FOREIGN KEY (int_id) REFERENCES job_win_db.Interview(id)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Correction(
-c_id int PRIMARY KEY,
+id varchar(60) PRIMARY KEY,
+created_at DATETIME,
+updated_at DATETIME,
 c_date date,
 text varchar(255),
 feedback varchar(255),
-int_id int,
-user_id int,
-coach_id int,
-FOREIGN KEY (int_id) REFERENCES job_win_db.Interview(int_id),
-FOREIGN KEY (coach_id) REFERENCES job_win_db.Users(user_id)
+int_id varchar(60),
+user_id varchar(60),
+coach_id varchar(60),
+FOREIGN KEY (user_id) REFERENCES job_win_db.Users(id),
+FOREIGN KEY (int_id) REFERENCES job_win_db.Interview(id),
+FOREIGN KEY (coach_id) REFERENCES job_win_db.Users(id)
 );
 
 
 CREATE TABLE IF NOT EXISTS job_win_db.Relation(
-user_id int PRIMARY KEY,
-dat date
+id_1 varchar(60),
+id_2 varchar(60),
+created_at DATETIME,
+updated_at DATETIME,
+FOREIGN KEY (id_1) REFERENCES job_win_db.Users(id),
+FOREIGN KEY (id_2) REFERENCES job_win_db.Users(id),
+PRIMARY KEY (id_1, id_2)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Sub_follow(
-user1_id int,
-user2_id int,
-FOREIGN KEY (user1_id) REFERENCES job_win_db.Users(user_id),
-FOREIGN KEY (user2_id) REFERENCES job_win_db.Users(user_id),
-dat date
+id_1 varchar(60),
+id_2 varchar(60),
+created_at DATETIME,
+updated_at DATETIME,
+FOREIGN KEY (id_1) REFERENCES job_win_db.Users(id),
+FOREIGN KEY (id_2) REFERENCES job_win_db.Users(id),
+PRIMARY KEY (id_1, id_2)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Comm_like(
-user_id int,
-com_id int,
-FOREIGN KEY (user_id) REFERENCES job_win_db.Users(user_id), 
-FOREIGN KEY (com_id) REFERENCES job_win_db.Comments(com_id)
+id_1 varchar(60),
+id_2 varchar(60),
+created_at DATETIME,
+updated_at DATETIME,
+FOREIGN KEY (id_1) REFERENCES job_win_db.Users(id), 
+FOREIGN KEY (id_2) REFERENCES job_win_db.Comments(id),
+PRIMARY KEY (id_1, id_2)
 );
 
 CREATE TABLE IF NOT EXISTS job_win_db.Post_like(
-user_id int,
-post_id int,
-FOREIGN KEY (user_id) REFERENCES job_win_db.Users(user_id),
-FOREIGN KEY (post_id) REFERENCES job_win_db.Post(post_id)
+id_1 varchar(60),
+id_2 varchar(60),
+created_at DATETIME,
+updated_at DATETIME,
+FOREIGN KEY (id_1) REFERENCES job_win_db.Users(id),
+FOREIGN KEY (id_2) REFERENCES job_win_db.Post(id),
+PRIMARY KEY (id_1, id_2)
 );
 
