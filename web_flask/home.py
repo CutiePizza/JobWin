cfc0f017b73e2fd2e7985679eed16d30d2804517
CutiePizza <ines.chokri97@gmail.com>
2020-06-15 14:00:02 +0100

    update

--- web_flask/home.py
+++ web_flask/home.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/home')
 def hello_hbnb():
     """ Prints a Message when / is called """
     return render_template('home.html')
